77168b8c32470e66fa6980eed8623e2173fe1b8e
Nomad07 <32042394+Nomad07@users.noreply.github.com>
2026-08-05 18:45:13 +0300

    Add division operation

--- calculator.py
+++ calculator.py
@@ -4,7 +4,9 @@ second = float(input("Enter second number: "))
 addition = first + second
 subtraction = first - second
 multiplication = first * second
+division = first / second
 
 print("Addition:", addition)
 print("Subtraction:", subtraction)
 print("Multiplication:", multiplication)
+print("Division:", division)
