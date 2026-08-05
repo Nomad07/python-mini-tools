43c926094705eaa34259b03c86167c493edebdd3
Nomad07 <32042394+Nomad07@users.noreply.github.com>
2026-08-05 19:37:52 +0300

    Add special characters to passwords

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,12 @@ import string
 
 length = int(input("Enter password length: "))
 
-characters = string.ascii_letters + string.digits
+characters = (
+    string.ascii_letters +
+    string.digits +
+    string.punctuation
+)
+
 password = ""
 
 for _ in range(length):
