3ddcc6729c789a291844388a5ed4357c097d3dcd
Nomad07 <32042394+Nomad07@users.noreply.github.com>
2026-08-06 23:49:54 +0300

    Add kilometer to meter conversion

--- unit_converter.py
+++ unit_converter.py
@@ -1,5 +1,14 @@
-meters = float(input("Enter meters: "))
+print("Choose conversion:")
+print("1 - Meters to Kilometers")
+print("2 - Kilometers to Meters")
 
-kilometers = meters / 1000
+choice = input("Your choice: ")
 
-print("Kilometers:", kilometers)
+if choice == "1":
+    meters = float(input("Enter meters: "))
+    print("Kilometers:", meters / 1000)
+elif choice == "2":
+    kilometers = float(input("Enter kilometers: "))
+    print("Meters:", kilometers * 1000)
+else:
+    print("Invalid choice")
