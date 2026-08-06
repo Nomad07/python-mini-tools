d60d8a48bda010a4864e1c3842ffff10e42bd269
Nomad07 <32042394+Nomad07@users.noreply.github.com>
2026-08-06 23:56:54 +0300

    Add line counter

--- text_analyzer.py
+++ text_analyzer.py
@@ -2,6 +2,8 @@ text = input("Enter some text: ")
 
 characters = len(text)
 words = len(text.split())
+lines = len(text.splitlines())
 
 print("Characters:", characters)
 print("Words:", words)
+print("Lines:", lines)
